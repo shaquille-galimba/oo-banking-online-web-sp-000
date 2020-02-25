7e40a3640eee6d8a0571c301cb5fcb64b4a23849
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-25 08:47:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if sender.balance> amount && valid? status == "complete"
+    if sender.balance> amount && valid? && status == "complete"
       sender.deposit(amount)
       receiver.deposit(-amount)
       @status = "reversed"
